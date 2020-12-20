66071c950cb9bc169eb7c6b66ac697972e9768c8
JIpuMa <71881475+JIpuMa@users.noreply.github.com>
2020-12-20 05:22:10 +0200

    Primak Hw14

---
+++ Hw14(5.0.1d).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef struct Address
+{
+	string sity;
+	string street;
+	unsigned int house;
+	unsigned int flat;
+};
+
+int main()
+{
+	Address address1;
+	return 0;
+}
